6e909612f8e3fa9c779445495dd6f2e8e0c1fbe4
hsjeon1224 <hsjeon1224@inbiznetcorp.com>
2015-09-10 15:14:28 +0900

    최초커밋

---
+++ app.js
@@ -0,0 +1,30 @@
+var http    =	require('http');
+var fs 		=	require('fs');
+var socketio = require('socket.io');
+//웹서버를 만듭니다.
+var server  = http.createServer(function(request, response) {
+	//htmlPage.html 파일을 읽습니다.
+	fs.readFile("index.html", function(err, data) {
+		response.writeHead(200, {"Content-Type":"text/html"});
+		response.end(data);
+	})
+}).listen(52273,function(){
+	console.log("Server running at http://127.0.0.1:52273");
+});
+var io = socketio.listen(server);
+
+io.sockets.on("connection", function (socket) {
+	console.log("연결.......................")
+	socket.on("touchmove", function (data) {
+		io.sockets.emit("touchmove", data);
+	});
+	socket.on("touchstart", function (data) {
+		io.sockets.emit("touchstart", data);
+	});
+	socket.on("touchend", function (data) {
+		io.sockets.emit("touchstart", data);
+	});
+	socket.on("resetCanvas", function (data) {
+		io.sockets.emit("resetCanvas", data);
+	});
+});
